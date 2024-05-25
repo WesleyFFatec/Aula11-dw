845a8614c327763447924dc767f2babe7a58ad12
Wesley Ferreira <wesleyff10@gmail.com>
2024-05-25 11:27:42 -0300

    ok
    ok

---
+++ js/variaveis.js
@@ -0,0 +1,42 @@
+// comentario de uma linda 
+/* comentario de multiplas linhas*/
+
+
+// tipos de variaveis 
+
+var num =1;
+var txt = "Ola"
+var logico_v= true 
+var logico_f = false 
+var lista_mesmo_tipo =['Arroz','Feijão','Batata']
+var lista_tipo_mesclado =['Nota',10,'Nota']
+var objeto = {
+    "nome": "Francisco",
+    "idade": 31,
+    "altura":1.78,
+    "situacao": true,
+    "linguage_fav": ['Java','JS','PHP']
+
+}
+
+var vazio = null;
+var indefenido;
+
+// exibindo o conteudo das variveis
+
+console.log(num, txt,logico_v,
+    logico_f,lista_mesmo_tipo,lista_tipo_mesclado,objeto,vazio,indefinido)
+
+
+// re-atribuição de valores 
+
+txt = 'Texto substituido'
+
+// re-atribuição de valores 
+
+logico_v= "Verdadeiro"
+
+
+
+
+    //escopo de variaveis //
